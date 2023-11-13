bfc1a0b24af304fc884b07293a3a8d913c376262
miyasan-09 <145628704+miyasan-09@users.noreply.github.com>
2023-11-13 11:50:41 +0900

    商品テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_020813) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_023334) do
   create_table "companies", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_020813) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+  end
+
+  create_table "products", charset: "utf8", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_products_on_company_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -39,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_020813) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "companies"
 end
