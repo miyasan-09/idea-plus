721e30b6ad7715b01de2916e1afab7fb41d73284
miyasan-09 <145628704+miyasan-09@users.noreply.github.com>
2023-11-14 21:48:45 +0900

    商品投稿

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :companies
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:index, :show] do
+  resources :products, only: [:index, :show, :create, :new] do
     resources :comments, only: [:create]
   end
 end
