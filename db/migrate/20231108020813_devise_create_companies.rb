013e505a0eef074fc22a3f986979f8375672cebe
miyasan-09 <145628704+miyasan-09@users.noreply.github.com>
2023-11-16 22:20:47 +0900

    企業ログイン

--- db/migrate/20231108020813_devise_create_companies.rb
+++ db/migrate/20231108020813_devise_create_companies.rb
@@ -4,6 +4,7 @@ class DeviseCreateCompanies < ActiveRecord::Migration[7.0]
   def change
     create_table :companies do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :phone_number,       null: false
